5a3b3bb6e20cbaf9343be263f71e847a79cddc72
Philipst77 <130857838+Philipst77@users.noreply.github.com>
2024-10-31 18:26:22 -0400

    java fx and half way through sql bs

---
+++ newimm-domain/src/main/java/org/openjfx/App.java
@@ -0,0 +1,9 @@
+package org.openjfx;
+
+import javafx.application.Application;
+
+public class App {
+    public static void main(String[] args) {
+        Application.launch(Login.class, args);
+    }
+}
